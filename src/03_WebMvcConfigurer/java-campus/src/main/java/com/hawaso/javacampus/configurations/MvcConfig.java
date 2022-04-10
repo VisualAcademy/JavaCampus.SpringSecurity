70b6ec0df1af5507f63e224887e95eb7eb0b10d6
Park YongJune <redplus@redplus.net>
2022-04-10 10:40:29 +0900

    MvcConfig

---
+++ src/03_WebMvcConfigurer/java-campus/src/main/java/com/hawaso/javacampus/configurations/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.hawaso.javacampus.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // 컨트롤러를 따로 만들지 않고 라우팅(경로)과 뷰 페이지 연결 
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+    }
+}
